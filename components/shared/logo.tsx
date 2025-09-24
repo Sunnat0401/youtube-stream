794329ac57faaa4726145066a9e6257bcb0e73c4
Sunnat0401 <sunnnatdasturchi@gmail.com>
2025-09-24 11:17:36 +0500

    Browse layout

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Logo = () => {
 	return (
 		<Link href={'/'} className='flex items-center space-x-2'>
-			<div className='text-3xl font-space_grotesk font-bold'>Sammi</div>
+			<div className='text-3xl font-space_grotesk font-bold'>Stream</div>
 		</Link>
 	)
 }
